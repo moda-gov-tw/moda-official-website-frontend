a7b24de325e0b090722e90d1a2a9916c39067827
robot <robot@moda.gov.tw>
2022-08-26 13:28:09 +0800

    'robotUpdate'

--- js/site.js
+++ js/site.js
@@ -54,6 +54,7 @@ var _JsData;
 
 function NewList(sqn) {
     var obj = getCookie("SearchObjN");
+obj ="";
     if (obj != "") {
         RemoveCookie("SearchObjN");
         var objJson = JSON.parse(obj);
