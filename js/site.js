b4036db4bf231bce0e5d9e9937af44375423852c
robot <robot@moda.gov.tw>
2024-03-19 13:11:09 +0800

    robotUpdate

--- js/site.js
+++ js/site.js
@@ -46,12 +46,26 @@ function webSiteLange(lan, webSiteId) {
 
         });
         $(".footer").load(lan + webSiteId + "/home/Footer.html", function () {
+            var now = formatDate(new Date());
+            $("#spanDateNow").prepend(now);
             FECommon.footerFtNavStyle();
             FECommon.widgetLazyload();
         });
     }
 }
+function formatDate(date) {
+    var d = new Date(date),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
 
+    if (month.length < 2)
+        month = '0' + month;
+    if (day.length < 2)
+        day = '0' + day;
+
+    return [year, month, day].join('-');
+}
 //search
 var _JsData;
 var foreverApi = 0;
@@ -166,7 +180,6 @@ function SearchAjax(obj) {
         "P": parseInt(obj.p),
         "DisplayCount": parseInt(obj.displaycount)
     };
-    console.log(data);
     $.ajax({
         url: Url,
         method: 'POST',
@@ -232,28 +245,30 @@ function SearchJsonData(p) {
         var S1 = $("#ns")[0].innerHTML;
         var S2 = $("#ca")[0].innerHTML;
         var lang = $(".webSitelanguage").attr("lang");
-        if (lang == "en") {
-            S1 = S1.replace(new RegExp("連結此問答", 'g'), "Link in context");
+        switch (lang) {
+            case "en":
+                S1 = S1.replace(new RegExp("連結此問答", 'g'), "Link in context");
+                S1 = S1.replace(new RegExp('lang="en"', 'g'), "");
+                if (_Module == "Bilingual") {
+                    S1 = S1.replace(new RegExp("#zhtw", 'g'), "#1en");
+                    S1 = S1.replace(new RegExp("#en", 'g'), "#zhtw0");
+                    S1 = S1.replace(new RegExp("#1en", 'g'), "#en");
+                    S1 = S1.replace(new RegExp("#zhtw0", 'g'), "#zhtw");
+                    S1 = S1.replace(new RegExp("#bzh", 'g'), "#b1en");
+                    S1 = S1.replace(new RegExp("#ben", 'g'), "#b1zh");
+                    S1 = S1.replace(new RegExp("#b1en", 'g'), "#ben");
+                    S1 = S1.replace(new RegExp("#b1zh", 'g'), "#bzh");
+                    S1 = S1.replace(new RegExp("雙語詞彙", 'g'), "Bilingual");
+                    S1 = S1.replace(new RegExp("序號", 'g'), "No.");
+                    S1 = S1.replace(new RegExp("詞彙", 'g'), "English");
+                    S1 = S1.replace(new RegExp("英譯文", 'g'), "Chinese");
+                }
+                break;
+            case "zh-tw":
+                S1 = S1.replace(new RegExp('lang="zh-tw"', 'g'), "");
+                break;
         }
-        if (_Module == "Bilingual" && lang == "en") {
-            S1 = S1.replace(new RegExp("#zhtw", 'g'), "#1en");
-            S1 = S1.replace(new RegExp("#en", 'g'), "#zhtw0");
-
-            S1 = S1.replace(new RegExp("#1en", 'g'), "#en");
-            S1 = S1.replace(new RegExp("#zhtw0", 'g'), "#zhtw");
-
-            S1 = S1.replace(new RegExp("#bzh", 'g'), "#b1en");
-            S1 = S1.replace(new RegExp("#ben", 'g'), "#b1zh");
-
-            S1 = S1.replace(new RegExp("#b1en", 'g'), "#ben");
-            S1 = S1.replace(new RegExp("#b1zh", 'g'), "#bzh");
-
-
-            S1 = S1.replace(new RegExp("雙語詞彙", 'g'), "Bilingual");
-            S1 = S1.replace(new RegExp("序號", 'g'), "No.");
-            S1 = S1.replace(new RegExp("詞彙", 'g'), "English");
-            S1 = S1.replace(new RegExp("英譯文", 'g'), "Chinese");
-        }
+        
         var displaycount = 15;
         if ($("#perPageShow").length > 0) {
             displaycount = $("#perPageShow").find(':selected').val();
